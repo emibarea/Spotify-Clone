a1288f3ecaceb4a6f54858e482eff2392a47bf6d
emiliano <emilianobarea8@gmail.com>
2022-08-15 18:58:16 -0300

    vercel prueba 3

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,15 +1,18 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
-export function middleware(req) {
-  const token = getToken({ req, secert: process.env.JWT_SECRET });
+export async function middleware(req) {
+  const url = req.nextUrl.clone();
+  const token = await getToken({ req, secret: process.env.JWT_SECRET });
 
   const { pathname } = req.nextUrl;
 
   if (pathname.includes("/api/auth") || token) {
     return NextResponse.next();
   }
+
   if (!token && pathname !== "/login") {
-    return NextResponse.rewrite(new URL("/login", req.url));
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
   }
 }
